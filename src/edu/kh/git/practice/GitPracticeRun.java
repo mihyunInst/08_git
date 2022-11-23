ebf4e4715f5e2425d32dfe7e31da2f7f9943f0bd
mihyunInst <cmhInst@gmail.com>
2022-11-23 16:41:45 +0900

    Test: 원격 푸시 확인

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -5,6 +5,9 @@ public class GitPracticeRun {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요~");
 		System.out.println("Git 연습용 파일입니다!");
+		
+		System.out.println("헉 너무 신기해요!");
+		System.out.println("정말로 GitHub에 내 코드가 나올까요?");
 	}
 
 }
